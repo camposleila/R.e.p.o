2db2f38c7278377acfd92297fd2012798de97b62
Leila Saraí Campos Peraza <00141525@uca.edu.sv>
2025-04-27 17:41:41 -0600

    Add files via upload

--- Calificacionaprobatoria.cpp
+++ Calificacionaprobatoria.cpp
@@ -6,29 +6,29 @@ int main()
 {
     double a;
     
-    cout << "Bienvenido. Inserte la calificación obtenida" <<endl;
+    cout << "Inserte la calificación obtenida" <<endl;
     cin >> a;
-    if (a>=90 && a<100)
+    if (90<a<100)
     {
         cout << "Clasificación: Excelente. Felicidades, obtuviste la mejor clasificación! :D" << endl;
     }
-    else if (a>=80 && a<=89)
+    else if (80<a<89)
     {
         cout << "Clasificación: Muy Bueno. Felicidades :) Sigue así" << endl;
     }
-    else if (a>=70 && a<=79)
+    else if (70<a<79)
     {
         cout << "Clasificación: Bueno. Felicidades. Aprobaste, sigue mejorando" << endl;
     }
-    else if (a>=60 && a<=69)
+    else if (60<a<69)
     {
         cout << "Clasificación: Regular. Aprobaste, puedes seguir mejorando" << endl;
     }
-    else if (a>=50 && a<=59)
+    else if (50<a<59)
     {
         cout << "Clasificación: Insuficiente. Debes seguir practicando. No te desanimes." << endl;
     }
-    else if (a>=0 && a<=49)
+    else if (0<a<49)
     {
         cout << "Clasificación: Reprobado. No lo has conseguido esta vez, sigue intentando." << endl;
     }
