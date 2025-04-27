0a14b3396182fcf321b275846c0f54d50290c870
Leila Saraí Campos Peraza <00141525@uca.edu.sv>
2025-04-27 17:50:04 -0600

    Update and rename Calificacionaprobatoria.cpp to 4Calificacionaprobatoria.cpp

---
+++ 4Calificacionaprobatoria.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    
+    cout << "Inserte la calificación obtenida" <<endl;
+    cin >> a;
+    if (a>=60 && a<100)
+    {
+        cout << "Aprobaste. Felicidades! :D" << endl;
+    }
+    else if (a<59 && a>0)
+    {
+        cout << "No aprobaste :( Sigue intentando " << endl;
+    }
+    else 
+    {
+        cout << "La nota ingresada no está dentro del rango. Intentalo de nuevo." <<endl;
+    }
+
+    cout << "Gracias por hacer uso de este programa. Bye." <<endl;
+    return 0;
+}
