19dc2c377753b352599505e82075901141473d16
Leila Saraí Campos Peraza <00141525@uca.edu.sv>
2025-06-03 08:36:44 -0600

    for5

---
+++ For_5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Cuenta regresiva
+
+int main()
+{
+    cout << "Cuenta regresiva" << endl;
+    for (int i = 10; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+    return 0; 
+}
