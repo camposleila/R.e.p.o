4d624cdf27cdb9144bd631cc1d52f601d6542c5a
camposleila <00118825@uca.edu.sv>
2025-06-05 09:43:14 -0600

    Add While_1.cpp

---
+++ While_1.cpp
@@ -0,0 +1,14 @@
+//Contador del 1 al 10
+#include <iostream>
+using namespace std; 
+
+int main (){
+    int contador = 0;
+    
+    while (contador <10)
+    {
+        contador = contador +1;
+        cout << contador << endl; 
+    }
+    return 0;
+}
